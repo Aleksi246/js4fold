03cb14dc44c2077e4190418c885f7ba45d82c22b
Aleksipc <aleklap@metropolia.figit config --global user.emailgit config --global user.email aleklap@metropolia.fi>
2025-10-30 19:03:04 +0200

    message

--- t1/t1.js
+++ t1/t1.js
@@ -1,5 +1,5 @@
 // array for todo list
-const todoList = [
+let todoList = [
   {
     id: 1,
     task: 'Learn HTML',
@@ -27,4 +27,92 @@ const todoList = [
   },
 ];
 
+const ul = document.querySelector('ul');
+
 // add your code here
+for (let i of todoList) {
+  let li = document.createElement('li');
+
+  let input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = 'todo-' + i.id;
+  input.checked = i.completed;
+
+  input.addEventListener('change', () => {
+    i.completed = !i.completed;
+    console.log(todoList);
+  });
+
+  let label = document.createElement('label');
+  label.htmlFor = 'todo-' + i.id;
+  label.innerText = i.task;
+
+  let butt = document.createElement('button');
+  butt.innerText = 'delete';
+
+  butt.addEventListener('click', () => {
+    todoList = todoList.filter((a) => a.id !== i.id);
+    console.log(todoList);
+    ul.removeChild(li);
+  });
+
+  li.appendChild(input);
+  li.appendChild(label);
+  li.appendChild(butt);
+
+  ul.appendChild(li);
+}
+
+const dialog = document.querySelector('#todo-dialog');
+
+document.querySelector('.add-btn').addEventListener('click', () => {
+  dialog.showModal();
+  input.focus();
+});
+
+let counter = 6;
+document.querySelector('#sub').addEventListener('click', () => {
+  event.preventDefault();
+
+  let newdata = {
+    id: counter,
+    task: document.querySelector('#input').value,
+    completed: false,
+  };
+
+  todoList.push(newdata);
+  dialog.close();
+
+  let li = document.createElement('li');
+
+  let input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = 'todo-' + counter;
+
+  input.addEventListener('change', () => {
+    newdata.completed = !newdata.completed;
+    console.log(todoList);
+  });
+
+  let label = document.createElement('label');
+  label.htmlFor = 'todo-' + newdata.id;
+  label.innerText = newdata.task;
+
+  let butt = document.createElement('button');
+  butt.innerText = 'delete';
+
+  butt.addEventListener('click', () => {
+    todoList = todoList.filter((a) => a.id !== newdata.id);
+    console.log(todoList);
+    ul.removeChild(li);
+  });
+
+  li.appendChild(input);
+  li.appendChild(label);
+  li.appendChild(butt);
+
+  ul.appendChild(li);
+
+  counter++;
+  console.log(todoList);
+});
